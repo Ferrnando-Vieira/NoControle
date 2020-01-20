952b6d9c5bd674a51e3a0391ce0ab537ec79ce97
hirazuky <nandovieira2@yahoo.com.br>
2020-01-20 02:12:38 -0300

    Adição da troca de frases no rodapé ao clicar nos botões

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function changeQuote(quoteId) {
+  var quoteTag = document.getElementById("quotes");
+
+  if (quoteId == "1") {
+    quoteTag.innerHTML = '"Frase 1"<b>Pessoa 1</b>';
+  } else if (quoteId == "2") {
+    quoteTag.innerHTML = '"Frase 2"<b>Pessoa 2</b>';
+  } else {
+    quoteTag.innerHTML = '"Frase 3"<b>Pessoa 3</b>';
+  }
+}
