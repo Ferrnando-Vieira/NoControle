e5c94eda968c1e61d45e2c2c11ec92b4d3a96fe0
NicolasMartins97 <58514827+NicolasMartins97@users.noreply.github.com>
2020-01-22 21:36:03 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-function changeQuote(quoteId) {
-  var quoteTag = document.getElementById("quotes");
-
-  if (quoteId == "1") {
-    quoteTag.innerHTML = '"Frase 1"<b>Pessoa 1</b>';
-  } else if (quoteId == "2") {
-    quoteTag.innerHTML = '"Frase 2"<b>Pessoa 2</b>';
-  } else {
-    quoteTag.innerHTML = '"Frase 3"<b>Pessoa 3</b>';
-  }
-}
-
 function download_pdf() {
   window.open('../files/carnavalNoControle.pdf', '_blank');
 }
